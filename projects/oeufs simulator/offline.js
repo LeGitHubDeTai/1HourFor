0fa28500f17b4bdc8213a64aa374cdcda811fd45
Tai Studio <tai.studio@outlook.fr>
2022-01-23 00:56:02 +0100

    🎬 video 2

---
+++ projects/oeufs simulator/offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1642895368,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/sprite-sheet0.png",
+		"images/wall.png",
+		"images/egg-sheet0.png",
+		"images/egg-sheet1.png",
+		"images/remover-sheet0.png",
+		"images/cross-sheet0.png",
+		"media/pop.m4a",
+		"media/pop.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
